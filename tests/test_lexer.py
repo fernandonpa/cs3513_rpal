1beff8f24cd4b955c41e57c5ac6da30292630f18
Deepthi Damruwan <deepthidamruvan@gmail.com>
2025-06-09 22:19:18 +0530

    tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,209 +1,99 @@
 import pytest
-from src.lexer.tokenizer import Tokenizer
-from src.lexer.token import Token
+from src.lexer.lexer import tokenize
 from src.lexer.token_types import TokenType
-from src.lexer.lexer_error import UnexpectedCharacterError, UnclosedStringError
 
-class TestTokenizer:
-    
-    def test_empty_input(self):
-        tokenizer = Tokenizer("")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 1
-        assert tokens[0].type == TokenType.EOF
-    
-    def test_whitespace(self):
-        tokenizer = Tokenizer("   \n\t   ")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 1
-        assert tokens[0].type == TokenType.EOF
-    
-    def test_single_keyword(self):
-        tokenizer = Tokenizer("let")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 2
-        assert tokens[0].type == TokenType.LET
-        assert tokens[0].value == "let"
-        assert tokens[1].type == TokenType.EOF
-    
-    def test_all_keywords(self):
-        keywords = [
-            "let", "in", "fn", "where", "aug", "or", "and", "not", 
-            "gr", "ge", "ls", "le", "eq", "ne", "true", "false", 
-            "nil", "dummy", "within", "rec"
-        ]
-        
-        for keyword in keywords:
-            tokenizer = Tokenizer(keyword)
-            tokens = tokenizer.tokenize()
-            assert len(tokens) == 2
-            assert tokens[0].value.lower() == keyword  # Case insensitive comparison
-            assert tokens[1].type == TokenType.EOF
-    
-    def test_identifiers(self):
-        tokenizer = Tokenizer("x y abc ABC abc123 a_b_c")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 7  # 6 identifiers + EOF
-        
-        assert tokens[0].type == TokenType.IDENTIFIER
-        assert tokens[0].value == "x"
-        
-        assert tokens[1].type == TokenType.IDENTIFIER
-        assert tokens[1].value == "y"
-        
-        assert tokens[2].type == TokenType.IDENTIFIER
-        assert tokens[2].value == "abc"
-        
-        assert tokens[3].type == TokenType.IDENTIFIER
-        assert tokens[3].value == "ABC"
-        
-        assert tokens[4].type == TokenType.IDENTIFIER
-        assert tokens[4].value == "abc123"
-        
-        assert tokens[5].type == TokenType.IDENTIFIER
-        assert tokens[5].value == "a_b_c"
-    
-    def test_integers(self):
-        tokenizer = Tokenizer("0 123 456789")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 4  # 3 integers + EOF
-        
-        assert tokens[0].type == TokenType.INTEGER
-        assert tokens[0].value == "0"
-        
-        assert tokens[1].type == TokenType.INTEGER
-        assert tokens[1].value == "123"
-        
-        assert tokens[2].type == TokenType.INTEGER
-        assert tokens[2].value == "456789"
-    
-    def test_strings(self):
-        tokenizer = Tokenizer("'hello' 'world' 'with \\'escape\\''")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 4  # 3 strings + EOF
-        
-        assert tokens[0].type == TokenType.STRING
-        assert tokens[0].value == "hello"
-        
-        assert tokens[1].type == TokenType.STRING
-        assert tokens[1].value == "world"
-        
-        # The escaped quotes would be handled at parse time
-        assert tokens[2].type == TokenType.STRING
-    
-    def test_operators(self):
-        tokenizer = Tokenizer("+ - * / -> = | > < !")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 11  # 7 operators + EOF
-        
-        assert tokens[0].type == TokenType.PLUS
-        assert tokens[0].value == "+"
-        
-        assert tokens[1].type == TokenType.MINUS
-        assert tokens[1].value == "-"
-        
-        assert tokens[2].type == TokenType.MULTIPLY
-        assert tokens[2].value == "*"
-        
-        assert tokens[3].type == TokenType.DIVIDE
-        assert tokens[3].value == "/"
-        
-        assert tokens[4].type == TokenType.ARROW
-        assert tokens[4].value == "->"
-        
-        assert tokens[5].type == TokenType.EQUALS
-        assert tokens[5].value == "="
-        
-        assert tokens[6].type == TokenType.PIPE
-        assert tokens[6].value == "|"
+# Helper function to extract (type, value) from token list
+def tokens_to_type_value_list(tokens):
+    return [(token.get_type(), token.get_value()) for token in tokens]
 
-        assert tokens[7].type == TokenType.GR
-        assert tokens[7].value == ">"
 
-        assert tokens[8].type == TokenType.LS
-        assert tokens[8].value == "<"
+def test_sum_of_first_n():
+    code = """
+    let rec SumOfFirstN (number) = 
+        number eq 0 -> 0 | number + SumOfFirstN(number - 1)
+    in
+    print(SumOfFirstN(100))
+    """
+    tokens = tokenize(code)
+    expected = [
+        (TokenType.KEYWORD, 'let'),
+        (TokenType.KEYWORD, 'rec'),
+        (TokenType.IDENTIFIER, 'SumOfFirstN'),
+        (TokenType.PUNCTUATION, '('),
+        (TokenType.IDENTIFIER, 'number'),
+        (TokenType.PUNCTUATION, ')'),
+        (TokenType.OPERATOR, '='),
+        (TokenType.IDENTIFIER, 'number'),
+        (TokenType.KEYWORD, 'eq'),
+        (TokenType.INTEGER, '0'),
+        (TokenType.OPERATOR, '->'),
+        (TokenType.INTEGER, '0'),
+        (TokenType.OPERATOR, '|'),
+        (TokenType.IDENTIFIER, 'number'),
+        (TokenType.OPERATOR, '+'),
+        (TokenType.IDENTIFIER, 'SumOfFirstN'),
+        (TokenType.PUNCTUATION, '('),
+        (TokenType.IDENTIFIER, 'number'),
+        (TokenType.OPERATOR, '-'),
+        (TokenType.INTEGER, '1'),
+        (TokenType.PUNCTUATION, ')'),
+        (TokenType.KEYWORD, 'in'),
+        (TokenType.IDENTIFIER, 'print'),
+        (TokenType.PUNCTUATION, '('),
+        (TokenType.IDENTIFIER, 'SumOfFirstN'),
+        (TokenType.PUNCTUATION, '('),
+        (TokenType.INTEGER, '100'),
+        (TokenType.PUNCTUATION, ')'),
+        (TokenType.PUNCTUATION, ')')
+    ]
+    assert tokens_to_type_value_list(tokens) == expected
 
-        assert tokens[9].type == TokenType.NOT
-        assert tokens[9].value == "!"
-    
-    def test_punctuation(self):
-        tokenizer = Tokenizer("( ) ; ,")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 5  # 4 punctuation + EOF
-        
-        assert tokens[0].type == TokenType.LEFT_PAREN
-        assert tokens[0].value == "("
-        
-        assert tokens[1].type == TokenType.RIGHT_PAREN
-        assert tokens[1].value == ")"
-        
-        assert tokens[2].type == TokenType.SEMICOLON
-        assert tokens[2].value == ";"
-        
-        assert tokens[3].type == TokenType.COMMA
-        assert tokens[3].value == ","
-    
-    def test_comments(self):
-        tokenizer = Tokenizer("let x // This is a comment\n= 5")
-        tokens = tokenizer.tokenize()
-        assert len(tokens) == 5  # LET + IDENTIFIER + EQUALS + INTEGER + EOF
-        
-        assert tokens[0].type == TokenType.LET
-        assert tokens[1].type == TokenType.IDENTIFIER
-        assert tokens[1].value == "x"
-        assert tokens[2].type == TokenType.EQUALS
-        assert tokens[3].type == TokenType.INTEGER
-        assert tokens[3].value == "5"
-        assert tokens[4].type == TokenType.EOF
-    
-    def test_line_and_column_tracking(self):
-        tokenizer = Tokenizer("let\nx =\n 5")
-        tokens = tokenizer.tokenize()
-        
-        assert tokens[0].line == 1  # 'let' is on line 1
-        assert tokens[0].column == 1
-        
-        assert tokens[1].line == 2  # 'x' is on line 2
-        assert tokens[1].column == 1
-        
-        assert tokens[2].line == 2  # '=' is on line 2
-        assert tokens[2].column == 3
-        
-        assert tokens[3].line == 3  # '5' is on line 3
-        assert tokens[3].column == 2
-    
-    def test_unclosed_string_error(self):
-        tokenizer = Tokenizer("'unclosed string")
-        with pytest.raises(UnclosedStringError):
-            tokenizer.tokenize()
-    
-    def test_unexpected_character_error(self):
-        tokenizer = Tokenizer("let x = `")
-        with pytest.raises(UnexpectedCharacterError):
-            tokenizer.tokenize()
-    
-    def test_complex_program(self):
-        program = """
-        let Sum(A) = Psum (A, Order A)
-        where rec Psum(T, N) = N eq 0 -> 0
-                              | Psum(T, N-1) + T N
-        in Print(Sum(1, 2, 3, 4, 5))
-        """
-        
-        tokenizer = Tokenizer(program)
-        tokens = tokenizer.tokenize()
-        
-        # Verify a few key tokens
-        token_values = [token.value for token in tokens if token.type != TokenType.EOF]
-        
-        assert "let" in token_values
-        assert "Sum" in token_values
-        assert "where" in token_values
-        assert "rec" in token_values
-        assert "Psum" in token_values
-        assert "eq" in token_values
-        assert "->" in token_values
-        assert "+" in token_values
-        assert "|" in token_values
-        assert "in" in token_values
-        assert "Print" in token_values
+
+def test_check_palindrome():
+    code = """
+    let CheckPalindrome number =
+        number eq Reverse(number, 0) -> 'Palindrome' | 'Not a Palindrome'
+        where rec Reverse(num, reversed) =
+            num gr 0 -> Reverse(num / 10, (reversed * 10 + (num - (num / 10) * 10))) | reversed
+    in
+    Print(CheckPalindrome 1221)
+    """
+    tokens = tokenize(code)
+    values = [t.get_value() for t in tokens]
+    print(values)
+    assert 'CheckPalindrome' in values
+    assert 'Reverse' in values
+    assert "'Palindrome'" in values
+    assert "'Not a Palindrome'" in values
+
+
+def test_find_greatest():
+    code = """
+    let FindGreatest num1 num2 num3 =
+        (num1 > num2 & num1 > num3) -> num1
+        | (num2 > num3) -> num2
+        | num3
+    in
+    Print(FindGreatest 5 (-9) 8)
+    """
+    tokens = tokenize(code)
+    values = [t.get_value() for t in tokens]
+    assert 'FindGreatest' in values
+    assert 'num1' in values
+    assert '>' in values
+
+
+def test_sum_list():
+    code = """
+    let Sum(A) = Psum (A,Order A ) 
+    where rec Psum (T,N) = N eq 0 -> 0 
+    | Psum(T,N-1)+T N 
+    in Print ( Sum (1,2,3,4,5) ) 
+    """
+    tokens = tokenize(code)
+    values = [t.get_value() for t in tokens]
+    assert 'Sum' in values
+    assert 'Psum' in values
+    assert 'Order' in values
+    assert 'eq' in values
+    assert 'in' in values
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,155 +1,65 @@
 import pytest
-import os
-import sys
-from typing import List
-
-# Add parent directory to path to import modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-from src.lexer.tokenizer import Tokenizer
+from src.lexer.lexer import tokenize
 from src.parser.parser import Parser
 from src.parser.node_types import NodeType
-from src.parser.parser_error import ParserError
 
-def parse_code(code):
-    """Helper function to parse RPAL code"""
-    tokenizer = Tokenizer(code)
-    tokens = tokenizer.tokenize()
+def parse_code(source_code):
+    tokens = tokenize(source_code)
     parser = Parser(tokens)
-    return parser.parse()
+    ast = parser.parse()
+    return parser.convert_ast_to_string_ast()
 
-def test_simple_let_expression():
-    ast = parse_code("let x = 5 in x")
-    
-    # Check that it's a LET node with two children
-    assert ast.node_type == NodeType.LET
-    assert ast.value == "let"
-    assert len(ast.children) == 2
-    
-    # Check the definition part (should be an EQUAL node)
-    def_node = ast.children[0]
-    assert def_node.node_type == NodeType.EQUAL
-    assert len(def_node.children) == 2
-    
-    # Check the variable part of the definition
-    var_node = def_node.children[0]
-    assert var_node.node_type == NodeType.IDENTIFIER
-    assert var_node.value == "x"
-    
-    # Check the value part of the definition
-    val_node = def_node.children[1]
-    assert val_node.node_type == NodeType.INTEGER
-    assert val_node.value == "5"
-    
-    # Check the body part of the let expression
-    body_node = ast.children[1]
-    assert body_node.node_type == NodeType.IDENTIFIER
-    assert body_node.value == "x"
+def test_sum_of_first_n():
+    source_code = """
+    let rec SumOfFirstN (number) = 
+        number eq 0 -> 0 | number + SumOfFirstN(number - 1)
+    in
+    print(SumOfFirstN(100))
+    """
+    output = parse_code(source_code)
+    assert output[0].endswith("let")
+    assert any("SUMOFFIRSTN" in line.upper() for line in output)
+    assert any("number" in line.lower() for line in output)
 
-def test_function_definition():
-    ast = parse_code("let f x y = x + y in f 1 2")
-    
-    # Check that it's a LET node
-    assert ast.node_type == NodeType.LET
-    
-    # The first child should be the function definition
-    fcn_def = ast.children[0]
-    assert fcn_def.node_type == NodeType.FCN_FORM
-    assert len(fcn_def.children) == 4  # function name, 2 params, body
-    
-    # Check function name
-    fcn_name = fcn_def.children[0]
-    assert fcn_name.node_type == NodeType.IDENTIFIER
-    assert fcn_name.value == "f"
-    
-    # Check parameters
-    param1 = fcn_def.children[1]
-    assert param1.node_type == NodeType.IDENTIFIER
-    assert param1.value == "x"
-    
-    param2 = fcn_def.children[2]
-    assert param2.node_type == NodeType.IDENTIFIER
-    assert param2.value == "y"
-    
-    # Function body should be an addition operation
-    fcn_body = fcn_def.children[3]
-    assert fcn_body.node_type == NodeType.PLUS
-    
-    # Check the application in the let body
-    application = ast.children[1]
-    assert application.node_type == NodeType.GAMMA
-    
-    # Should be applying f to 1
-    fcn_app = application.children[0]
-    assert fcn_app.node_type == NodeType.GAMMA
-    
-    # The function being applied should be f
-    f_node = fcn_app.children[0]
-    assert f_node.node_type == NodeType.IDENTIFIER
-    assert f_node.value == "f"
-
-def test_tuple_creation():
-    ast = parse_code("(1, 2, 3)")
-    
-    # Should create a TAU node with 3 children
-    assert ast.node_type == NodeType.TAU
-    assert len(ast.children) == 3
-    
-    for i, child in enumerate(ast.children):
-        assert child.node_type == NodeType.INTEGER
-        assert child.value == str(i + 1)
-
-def test_conditional1():
-    ast = parse_code("x > 0 -> 'positive' | 'non-positive'")
-    
-    # Should be a conditional node
-    assert ast.node_type == NodeType.CONDITIONAL
-    assert len(ast.children) == 3
-    
-    # Check condition
-    condition = ast.children[0]
-    assert condition.node_type == NodeType.GT
-    
-    # Check true branch
-    true_branch = ast.children[1]
-    assert true_branch.node_type == NodeType.STRING
-    assert true_branch.value == "positive"
-    
-    # Check false branch
-    false_branch = ast.children[2]
-    assert false_branch.node_type == NodeType.STRING
-    assert false_branch.value == "non-positive"
+def test_check_palindrome():
+    source_code = """
+    let CheckPalindrome number =
+        number eq Reverse(number, 0) -> 'Palindrome' | 'Not a Palindrome'
+        where rec Reverse(num, reversed) =
+            num gr 0 -> Reverse(num / 10, (reversed * 10 + (num - (num / 10) * 10))) | reversed
+    in
+    Print(CheckPalindrome 1221)
+    """
+    output = parse_code(source_code)
+    assert any("CHECKPALINDROME" in line.upper() for line in output)
+    assert any("REVERSE" in line.upper() for line in output)
+    assert any("PALINDROME" in line.upper() for line in output)
 
 
-def test_where_expression():
-    ast = parse_code("x + y where x = 1 and y = 2")
-    
-    # Should be a WHERE node
-    assert ast.node_type == NodeType.WHERE
-    assert len(ast.children) == 2
-    
-    # Check expression
-    expr = ast.children[0]
-    assert expr.node_type == NodeType.PLUS
-    
-    # Check definition part
-    defs = ast.children[1]
-    assert defs.node_type == NodeType.AND
-    assert len(defs.children) == 2
+def test_find_greatest():
+    source_code = """
+    let FindGreatest num1 num2 num3 =
+        (num1 > num2 & num1 > num3) -> num1
+        | (num2 > num3) -> num2
+        | num3
+    in
+    Print(FindGreatest 5 (-9) 8)
+    """
+    output = parse_code(source_code)
+    assert "let" in [line.strip().lower() for line in output]
+    assert any("FINDGREATEST" in line.upper() for line in output)
+    assert any(">" in line or "gr" in line.lower() for line in output)
 
-def test_parser_error():
-    with pytest.raises(ParserError):
-        parse_code("let x = in x")  # Missing expression after =
-        
-    with pytest.raises(ParserError):
-        parse_code("x + ")  # Missing right operand
+def test_sum_list():
+    source_code = """
+    let Sum(A) = Psum (A,Order A ) 
+    where rec Psum (T,N) = N eq 0 -> 0 
+    | Psum(T,N-1)+T N 
+    in Print ( Sum (1,2,3,4,5) ) 
+    """
+    output = parse_code(source_code)
+    assert any("SUM" in line.upper() for line in output)
+    assert any("PSUM" in line.upper() for line in output)
+    assert any("ORDER" in line.upper() for line in output)
+    assert any("let" in line.lower() for line in output)
 
-if __name__ == "__main__":
-    test_simple_let_expression()
-    test_function_definition()
-    test_tuple_creation()
-    test_conditional1()
-    test_where_expression()
-    test_parser_error()
-    print("All parser tests passed!")
